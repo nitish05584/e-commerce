3a9b374a81900559c7fb6ed7266945cd14c7d7d0
Nitish <your_github_email@example.com>
2026-02-27 18:48:52 +0530

    nav

--- frontend/src/component/Nav.jsx
+++ frontend/src/component/Nav.jsx
@@ -1,15 +1,33 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import logo from "../assets/logo.png"
 import { IoSearchCircleOutline } from "react-icons/io5";
 import { FaRegUserCircle } from "react-icons/fa";
 
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { userDataContext } from '../context/UserContext';
+import { IoSearchCircleSharp } from "react-icons/io5";
+import { useNavigate } from 'react-router-dom';
+import { authDataContext } from '../context/AuthContext';
+import axios from 'axios';
 
 function Nav() {
-  let {userData}=useContext(userDataContext)
+  let navigate=useNavigate()
+  let {getCurrentUser,userData}=useContext(userDataContext)
+  let {serverUrl}=useContext(authDataContext)
+  let [showSearch,setShowSearch]=useState(false)
+   let [showProfile,setShowProfile]=useState(false)
+
+   const handleLogout=async()=>{
+    try {
+      const result=await axios.get(serverUrl + "/api/auth/logout",{withCredentials:true})
+      console.log(result.data);
+      getCurrentUser();
+    } catch (error) {
+      console.log(error)
+    }
+   }
   return (
-    <div className='w-[100vw] h-[70px] bg-white z-10 fixed top-0 flex items-center justify-between px-[30px] shadow-md shadow-black'>
+    <div className='w-[100vw] h-[70px] bg-white z-10 fixed top-0 flex items-center justify-between px-[30px] shadow-md shadow-black '>
 
       <div className='w-[30%] flex items-cneter justify-start gap-[10px]'>
         <img src={logo} alt=""className='w-[35px]'/>
@@ -29,17 +47,43 @@ function Nav() {
 
 
       <div className='w-[30%] flex items-center justify-end gap-[20px]'>
-         <IoSearchCircleOutline className='w-[38px] h-[38px] text-[#000000] cursor-pointer' />
 
-       { !userData && <FaRegUserCircle className='w-[28px] h-[28px] text-[#000000] cursor-pointer'/>}
+         {!showSearch && <IoSearchCircleOutline className='w-[38px] h-[38px] text-[#000000] cursor-pointer'onClick={()=>setShowSearch(prev=>!prev)} />
+}
+           {showSearch &&  <IoSearchCircleSharp className='w-[38px] h-[38px] text-[#000000] cursor-pointer'onClick={()=>setShowSearch(prev=>!prev)} />}
 
-      {userData && <div className='w-[30px] h-[30px] bg-[#080808] text-[white] rounded-full flex items-center justify-center'>{userData?.name.slice(0,1)}</div>}
+
+
+       { !userData && <FaRegUserCircle className='w-[28px] h-[28px] text-[#000000] cursor-pointer' onClick={()=>setShowProfile(prev=>!prev)}/>}
+
+      {userData && <div className='w-[30px] h-[30px] bg-[#080808] text-[white] rounded-full flex items-center justify-center cursor-pointer' onClick={()=>setShowProfile(prev=>!prev)}>{userData?.name.slice(0,1)}</div>}
 
          <MdOutlineShoppingCart className='w-[38px] h-[38px] text-[#000000] cursor-pointer ' />
          <p className='absolute w-[18px] h-[18px] itemse-center md:flex justify-center bg-black px-[5px] py-[2px] text-white rounded-full text-[9px] top-[10px] right-[23px] hidden'>10</p>
-
-
       </div>
+
+      {showSearch && <div className='w-[100%] h-[80px] bg-[#d8f6f9dd] absolute top-[100%] left-0 right-0 flex items-center justify-center'>
+       <input type="text" className='w-[50%] h-[60%] bg-[#233533] rounded-[30px] px-[50px] placeholder:text-white text-[white] text-[18px 'placeholder='Search here' />
+      </div>}
+
+
+
+ 
+      { showProfile && <div className='absolute w-[220px] h-[150px] bg-[#000000d7] top-[110%] right-[4%] border-[1px] border-[#aaa9a9] rounded-[10px] z-10'>
+
+        <ul className='w-[100%] h-[100%] flex items-start justify-around flex-col text-[17px] py-[10px] text-[white] '>
+
+         {!userData &&<li className='w-[100%] hover:bg-[#2f2f2f] px-[15px] py-[10px] cursor-pointer'onClick={()=>{navigate("/login"); setShowProfile(false)}}>Login</li>}
+
+         {userData &&<li className='w-[100%] hover:bg-[#2f2f2f] px-[15px] py-[10px] cursor-pointer'onClick={()=>{handleLogout(); setShowProfile(false)}}>Logout</li>}
+
+         <li className='w-[100%] hover:bg-[#2f2f2f] px-[15px] py-[10px] cursor-pointer'>Orders</li>
+
+         <li className='w-[100%] hover:bg-[#2f2f2f] px-[15px] py-[10px] cursor-pointer'>About</li>
+
+        </ul>
+
+      </div>}
       
     </div>
   )
