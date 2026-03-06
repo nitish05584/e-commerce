ceac3748441a37cf549ecfab62869f688287951b
Nitish <your_github_email@example.com>
2026-03-06 20:56:07 +0530

    sidebar

--- admin/src/pages/Home.jsx
+++ admin/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Nav from '../component/Nav'
+import Sidebar from '../component/Sidebar'
 
 function Home() {
   return (
-    <div>
-      ram
+    <div className='w-[100vw] h-[100vh] bg-gradient-to-b from-[#141414] to-[#0c2025] text-[white] relative'>
+      <Nav/>
+      <Sidebar/>
     </div>
   )
 }
