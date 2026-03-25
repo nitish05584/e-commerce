e49f5dea7c0d7b52238c1b2ffe496879b8b2a1af
nitish05584 <devnath8471@gmail.com>
2026-03-25 16:50:29 +0530

    Footer

--- frontend/src/component/Footer.jsx
+++ frontend/src/component/Footer.jsx
@@ -8,11 +8,53 @@ const Footer = () => {
 
             <div className='md:w-[30%] w-[35%] h-[100%] flex items-start justify-center flex-col gap-[5px]'>
                 <div className='flex items-start justify-start gap-[5px] mt-[10px] md:mt-[40px]'>
-                    <img src= {logo} alt=""/>
+                    <img src= {logo} alt="" className='md:w-[40px] md:h-[40px] w-[30px] h-[30px]'/>
+
+                    <p className='text-[19px] md:text-[20px] text-[black]'>OneCart</p>
+                   
                 
             </div>
+             <p className='text-[15px] text-[#1e2223] hidden md:block'>OneCart is your all-in-one online shopping destination, offering top-quality products, unbeatable deals, ans fast delivery-all backed by trusted service designed to make your life easier every day</p>
+
+                    <p className='text-[15px] text-[#1e2223] flex md:hidden'>Fast ,Easy . Reliable. OneCart Shopping</p>
+
+         
 
             </div>
+               <div className='md:w-[25%] w-[30%] h-[100%] flex items-center justify-center flex-col text-center'>
+                <div className='flex items-center justify-center gap-[5px] mt-[10px] md:mt-[40px]'>
+                 <p className='text-[19px] md:text-[20px] text-[#1e2223] font-sans'>COMPANY</p>
+                </div>
+                <ul>
+                <li className='text-[15px] text-[#1e2223] hidden md:block cursor-pointer'>Home</li>
+                <li className='text-[15px] text-[#1e2223] cursor-pointer'>About us</li>
+
+                <li className='text-[15px] text-[#1e2223] cursor-pointer'>Delivery</li>
+
+                <li className='text-[15px] text-[#1e2223] cursor-pointer'>Privacy Policy</li>
+               
+                </ul>
+            </div>
+
+            <div className='w-[40%] h-[100%] flex items-center justify-center flex-col text-center'>
+                  <div className='flex items-center justify-center gap-[5px] mt-[10px] md:mt-[40px]'>
+                 <p className='text-[19px] md:text-[20px] text-[#1e2223] font-sans'>GET IN TOUCH</p>
+                </div>
+                <ul>
+                <li className='text-[15px] text-[#1e2223] hidden md:block cursor-pointer'>+91-9576536863</li>
+                <li className='text-[15px] text-[#1e2223] cursor-pointer'>contact@fhfodrp</li>
+
+                <li className='text-[15px] text-[#1e2223] cursor-pointer'>+1-123-456-789</li>
+
+                <li className='text-[15px] text-[#1e2223] cursor-pointer'>admin@oneCart.com</li>
+               
+                </ul>
+            </div>
+        </div>
+
+        <div className='w-[100%] h-[1px] bg-slate-400'></div>
+        <div className='w-[100%] h-[5vh] bg-[#dbfcfcec] flex items-center justify-center'>Copyright 2026@onecart.com-All Rights Reserved
+
         </div>
       
     </div>
