79235f6cd0a2dd73521c9b4bb11dcbb8b1a32d4a
nitish05584 <devnath8471@gmail.com>
2026-04-22 20:44:24 +0530

    push

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext } from 'react'
 export const authDataContext=createContext()
 function AuthContext({children}) {
-let serverUrl="https://e-commerce-odl1.onrender.com"
+let serverUrl="http://localhost:8080"
     let value={
        serverUrl
     }
